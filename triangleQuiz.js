6f7aec27ae9091ddb7ef70af883a0c87ca6cf6f9
Deven Umrania <32352481+deven10@users.noreply.github.com>
2022-10-12 23:47:56 +0530

    Triangle Quiz added

---
+++ triangleQuiz.js
@@ -0,0 +1,20 @@
+const quizForm = document.querySelector(".quiz");
+const btn = document.querySelector("#btn");
+const output = document.querySelector("#output");
+
+const answers = ["90°", "Right angled"];
+
+function calculateCorrectAnswerScore(){
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === answers[index]){
+            score = score + 1;
+        } 
+            index = index + 1;
+    }
+    output.innerText = "Your score is " + score;
+}
+
+btn.addEventListener("click", calculateCorrectAnswerScore);
