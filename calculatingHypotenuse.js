2276cc47c082331d0275e2d5ba52bee43234a464
Deven Umrania <32352481+deven10@users.noreply.github.com>
2022-10-13 00:25:04 +0530

    CalculatingHypotenuse

---
+++ calculatingHypotenuse.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll(".triangle-side");
+const btn = document.querySelector("#btn");
+const output = document.querySelector("#output");
+
+function calculateSumOfSquares(a,b){
+    const sum = a*a + b*b;
+    return sum;
+}
+
+function calculateHypotenuse(){
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+    const lengthOfHypotenuse = (Math.sqrt(sumOfSquares)).toFixed(2);
+    output.innerText = "The square root is " + lengthOfHypotenuse;
+}
+
+btn.addEventListener("click", calculateHypotenuse);
