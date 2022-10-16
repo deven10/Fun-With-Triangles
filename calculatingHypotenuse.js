9a68d847736bce9d3bacae5587b0f56c1389227a
Deven Umrania <32352481+deven10@users.noreply.github.com>
2022-10-16 21:04:22 +0530

    Solved Negative Inputs Issue

--- calculatingHypotenuse.js
+++ calculatingHypotenuse.js
@@ -1,16 +1,27 @@
 const sides = document.querySelectorAll(".triangle-side");
 const btn = document.querySelector("#btn");
 const output = document.querySelector("#output");
+const firstSide = document.getElementById("first-side");
+const secondSide = document.getElementById("second-side");
 
 function calculateSumOfSquares(a,b){
     const sum = a*a + b*b;
     return sum;
 }
+console.log("lets go")
 
 function calculateHypotenuse(){
-    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
-    const lengthOfHypotenuse = (Math.sqrt(sumOfSquares)).toFixed(2);
-    output.innerText = "The Lenght of the Hypotenuse is " + lengthOfHypotenuse;
+    console.log((Number(firstSide.value)),(Number(secondSide.value)));
+    if(Number(firstSide.value) < 0 || Number(secondSide.value < 0)){
+        console.log("working");
+        output.innerText = "Please Enter Valid Inputs!";
+    } 
+    else{
+        console.log("not negative");
+        const sumOfSquares = calculateSumOfSquares(Number(firstSide.value), Number(secondSide.value));
+        const lengthOfHypotenuse = (Math.sqrt(sumOfSquares)).toFixed(2);
+        output.innerText = "The Lenght of the Hypotenuse is " + lengthOfHypotenuse;
+    }
 }
 
 btn.addEventListener("click", calculateHypotenuse);
